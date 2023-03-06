9f199ccc20996c431381c474f23acd0c235f0289
Mirko van Willigenburg <mirko@mediamonks.com>
2023-03-06 14:00:33 +0100

    updates to new framework functions

--- src/plain/shared/script/main.js
+++ src/plain/shared/script/main.js
@@ -1,32 +1,75 @@
-(function() {
-  // below line will be replaced by 'const config = {...[the whole richmediaRc]}'
-  // so you can still use all the settings/content like you normally would, i.e. config.settings.size.width
+import fitText from '@mediamonks/display-temple/util/fitText';
+import enableAdsRecorder from '@mediamonks/display-temple/util/enableRecorder';
+
+async function politeLoadImages(element) {
+  const imageList = Array.from(element.querySelectorAll('img[data-src]'));
+  const imageUrlList = imageList.map((img) => img.getAttribute('data-src'));
+
+  const result = await Promise.all(
+    imageUrlList.map((src) => {
+      return new Promise((resolve, reject) => {
+        const img = document.createElement('img');
+        img.onload = function () {
+          resolve(this);
+        };
+        img.onerror = reject;
+        img.dataset.src = src;
+        img.src = src;
+      });
+    }),
+  );
+
+  result.forEach((img, index) => {
+    imageList[index].src = img.src;
+  });
+}
+
+function createAnimation(obj, config) {
+  const tl = gsap.timeline(obj);
+  tl.to('.content', {duration: 0.5, opacity: 1});
+  return tl;
+}
+
+function createCtaAnimation(obj, config) {
+  const tl = gsap.timeline(obj);
+  tl.fromTo('.cta', {border: '0px solid black'}, {duration: 0.1, border: '1px solid black', backgroundColor: '#fff'});
+  tl.to('.cta_copy', {duration: 0.1, color: '#000'}, '<');
+
+
+  return tl;
+}
+
+
+(async () => {
   const config = 'webpackWillReplaceThisWithConfig';
-  
-  let timeline;
 
-  function createAnimation() {
-    const tl = gsap.timeline({paused: true});
+  // create timelines
+  const timeline = createAnimation({paused: true}, config);
+  const ctaTimeline = createCtaAnimation({paused: true}, config);
 
-    tl.to('.content', { duration: 1, opacity: 1 })
-
-    return tl;
-  }
-
-  function handleExit() {
+  // create exit / rollover events
+  const mainExit = document.querySelector('.mainExit');
+  mainExit.addEventListener('click', () => {
     window.open(window.clickTag, '_blank');
     timeline.progress(1);
-  }
+  });
 
-  function init() {
-    document.querySelector('.mainExit').addEventListener('click', handleExit);
+  mainExit.addEventListener('mouseover', () => {
+    ctaTimeline.play();
+  });
 
-    timeline = createAnimation();
-    timeline.play();
-  }
+  mainExit.addEventListener('mouseout', () => {
+    ctaTimeline.reverse();
+  });
 
-  window.onload = function() {
-    init();
-  }
-  
+  // await loading images and others
+  await politeLoadImages(document.querySelector('.banner'));
+  await document.fonts.ready;
+
+  fitText(document.querySelector('.title'), document.querySelector('.cta'))
+
+  enableAdsRecorder(timeline, config);
+
+  //play banner ad animation
+  timeline.play();
 })();
